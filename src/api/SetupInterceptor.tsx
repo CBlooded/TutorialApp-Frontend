59a09d189ca129ca879106f76c455418def64c23
Ereffe <eriffe9@gmail.com>
2025-05-15 15:47:40 +0200

    Refactor axios interceptors to use token from localStorage and update baseURL to 8080

--- src/api/SetupInterceptor.tsx
+++ src/api/SetupInterceptor.tsx
@@ -1,12 +1,12 @@
 import axiosConfig from "./axiosConfig";
-import axios from "axios";
+//import axios from "axios";
 
 const setupInterceptors = () => {
   axiosConfig.interceptors.request.use(
     (config) => {
-      const accessToken = localStorage.getItem("accessToken");
-      if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
       }
       return config;
     },
@@ -18,25 +18,27 @@ const setupInterceptors = () => {
       return response;
     },
     async (error) => {
-      const originalRequest = error.config;
-      if (error.response.status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
-        const refreshToken = localStorage.getItem("refreshToken"); //tbc
-        if (refreshToken) {
-          try {
-            const response = await axiosConfig.post(`/refreshToken`, {
-              //tbc
-              refreshToken,
-            });
-            const newAccessToken = response.data.accessToken;
-            localStorage.setItem("accessToken", newAccessToken);
-            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-            return axios(originalRequest);
-          } catch (error) {
-            console.log(`Token failed: ${error}`);
-          }
-        }
-      }
+      if (error.response.status === 401) console.log("Unauthorized");
+      //no refresh tokens yet
+      // const originalRequest = error.config;
+      // if (error.response.status === 401 && !originalRequest._retry) {
+      //   originalRequest._retry = true;
+      //   const refreshToken = localStorage.getItem("refreshToken");
+      //   if (refreshToken) {
+      //     try {
+      //       const response = await axiosConfig.post(`/refreshToken`, {
+      //         //tbc
+      //         refreshToken,
+      //       });
+      //       const newAccessToken = response.data.accessToken;
+      //       localStorage.setItem("accessToken", newAccessToken);
+      //       originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+      //       return axios(originalRequest);
+      //     } catch (error) {
+      //       console.log(`Token failed: ${error}`);
+      //     }
+      //   }
+      // }
       return Promise.reject(error);
     }
   );
--- src/api/axiosConfig.tsx
+++ src/api/axiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:3000", //base url for connecting with backend //to be changed when endpoints will be done
+  baseURL: "http://localhost:8080", //base url for connecting with backend //to be changed when endpoints will be done
   headers: {
     "Content-Type": "application/json", //same
   },
