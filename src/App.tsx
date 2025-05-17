9e05c364d97d44adc71c5b78a4758d6ef73dd265
SharpP03 <Patrykstoncel186@wp.pl>
2025-05-18 00:55:15 +0200

    Added NavBar component for navigation. +imported logo +example page "dashboard"

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import "./App.css";
 
 import Login from "./components/login";
-import Layout from "./components/Layout";
+// import Layout from "./components/Layout";
 import Register from "./components/register";
+import NavBar from "./components/NavBar";
+import Dashboard from "./pages/dashboard";
+
 import { BrowserRouter, Routes, Route } from "react-router";
+// import { Children } from "react";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="app" element={<Layout />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+    <NavBar />
+      <Routes>
+        <Route path="/">
+        <Route index element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,30 @@
+import "./NavBar.css";
+import logo from "../assets/logo.png";
+
+function NavBar() {
+  /***
+   * Main Navigation Bar
+   * Always visible on the top of the page.
+   *
+   * @returns {JSX.Element} - Returns the navigation bar component.
+   */
+
+  return (
+    <>
+      <header className="barContainer">
+        <img src={logo} alt="Logo" className="logoNav" />
+        <div className="navLinks">
+          <a href="/">Login</a>
+          <a href="/register">Register</a>
+          <a href="/dashboard">Dashboard</a>
+        </div>
+      </header>
+      {/* TO DO - current page location - breadcrumbs */}
+      {/* <div className="breadCrumbs">
+        <p>Home / Dashboard</p>
+    </div> */}
+    </>
+  );
+}
+
+export default NavBar;
